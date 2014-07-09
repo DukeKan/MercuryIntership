8f19f547853d7613934a55794565a9cfc4ac17fe
DukeKan <denzelpain@gmail.com>
2014-07-09 17:57:22 +0400

    На активити HomeActivity добавлен список с заголовком.

--- SplashListApp/src/com/example/splashlistapp/HomeActivity.java
+++ SplashListApp/src/com/example/splashlistapp/HomeActivity.java
@@ -2,14 +2,39 @@ package com.example.splashlistapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
+
+	private final byte sizeOfItemsList = 20;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_home_activity);
+
+		// получаем экземпляр элемента ListView
+		ListView lv = (ListView) findViewById(R.id.listOfItems);
+
+		// определяем массив типа String
+		final String[] items = new String[sizeOfItemsList];
+
+		for (int i = 0; i < sizeOfItemsList; i++) {
+			items[i] = "item" + i;
+		}
+
+		// используем адаптер данных
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, items);
+
+		View header = getLayoutInflater().inflate(R.layout.list_header, null);
+		((TextView) header.findViewById(R.id.tvText)).setText("Заголовок");
+
+		lv.addHeaderView(header);
+		lv.setAdapter(adapter);
+
 	}
 }
