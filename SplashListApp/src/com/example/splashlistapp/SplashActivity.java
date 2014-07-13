7a8e744d5ae4e074b05a023a426f887a1e8f5852
DukeKan <denzelpain@gmail.com>
2014-07-13 20:06:07 +0400

    HomeActivity is started by special timer. This timer is only exist and run when SplashActivity is in focus (it starts when SplashActivity starts (for example, when rotating device) and stops when SplashActivity closed (or even paused)). Because of restarting SplashActivity and timer when rotating, time for starting second activity needs to be saved/loaded. If user returns to SplashScreen from HomeScreen, application closes. For testing, timer is up for 10 seconds.

--- SplashListApp/src/com/example/splashlistapp/SplashActivity.java
+++ SplashListApp/src/com/example/splashlistapp/SplashActivity.java
@@ -1,34 +1,100 @@
 package com.example.splashlistapp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
 
-	public final int FLASH_TIME = 2000;
-	
+	/**
+	 * How many milliseconds will be SplashScreen shown on display 
+	 */
+	public int SPLASH_TIME = 10000;
+	/**
+	 * When was application started 
+	 */
+	private long startTime = 0;
+	/**
+	 * Tag for own log
+	 */
+	final String LOG_TAG = "myLogs";
+	/**
+	 * Timer that will start HomeActivity
+	 */
+	private Timer timer;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_splash_activity);
+		this.startTime = System.currentTimeMillis();
+	}
+
+	private void createTimer() {
+		timer = new Timer();
+		// What does the timer do?
+		TimerTask task = new TimerTask() {
+			public void run() {
+				// Start HomeActivity
+				startActivity(new Intent(getApplicationContext(),
+						HomeActivity.class));
+				// Stop timer
+				timer.cancel();
+			}
+		};
+		timer.schedule(task, this.SPLASH_TIME);
+		Log.d(LOG_TAG, String.valueOf(this.SPLASH_TIME));
 	}
 	
-	Thread splashScreen = new Thread(){
-		public void run(){
-				try{
-					sleep(FLASH_TIME);
-				}catch(InterruptedException e){
-					e.printStackTrace();
-				}finally{
-					startActivity(new Intent(getApplicationContext(),HomeActivity.class));
-					finish();
-				}
-		}
-		
-	};
-	{
-		splashScreen.start();
+	// At the second time we see this screen application must be stopped
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+		this.finish();
+	}
+
+	// Destroy timer if activity was closed
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    this.timer.cancel();
 	}
 	
+	
+	// Starts after onRestoreInstanceState()
+	@Override
+	protected void onResume() {
+		super.onResume();
+		createTimer();
+	}
+
+	
+	/* 
+	 * Need for load how many milliseconds remained for starting HomeActivity
+	 */
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		this.SPLASH_TIME = savedInstanceState.getInt("time_left");
+	}
+
+	
+	
+	/* 
+	 * Need for save how many milliseconds remained for starting HomeActivity
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// Home many milliseconds remained for starting HomeActivity
+		outState.putInt("time_left",
+				this.SPLASH_TIME
+						- (int) (System.currentTimeMillis() - this.startTime));
+		timer.cancel();
+	}
+
 }
